38acc409c1e842f8c925ab632cf134a42880f79f
Jarvis <jarvis@localhost.localdomain>
2017-10-18 20:40:25 -0300

    Scripts do Curso Programação para Redes-IFRN

---
+++ 06_slice.py
@@ -0,0 +1,17 @@
+x='python'
+print(x[0]) #retorno 'p'
+print(x[-1]) #retorno 'n'
+print(x[1:4]) #retorno 'yth'
+
+print(x[2:])
+print(x[:3])
+
+s='abcdefghijklmnop'
+print(s[1:10:2])
+#retorno 'bdfhj'
+
+print(s[::2])
+#retorno 'acegikmo'
+
+p = 'alo'
+print(p[::-1])
